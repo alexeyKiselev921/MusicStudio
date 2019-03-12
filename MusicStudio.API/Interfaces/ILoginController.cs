7be0072f89648649e53fb2f7f8c508d8ed3fd480
Алексей <@DESKTOP-J2QN423>
2019-03-12 22:09:31 +0200

    Initial projects

---
+++ MusicStudio.API/Interfaces/ILoginController.cs
@@ -0,0 +1,16 @@
+﻿using MusicStudio.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStudio.API.Interfaces
+{
+    interface ILoginController
+    {
+        void SignIn(User user);
+        bool TrySignIn(User user);
+        void SignUp();
+        bool TrySignUp();
+    }
+}
